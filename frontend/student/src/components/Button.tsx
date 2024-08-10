43ccb48023d20b1cfa3c22698c1935c411ccb68e
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2024-08-11 01:37:51 +0530

    button

---
+++ frontend/student/src/components/Button.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@material-tailwind/react"
+
+function ButtonGroup() {
+  return (
+    <Button
+    fullWidth
+        
+          
+          children={"login"} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}    >
+    
+    </Button>
+  )
+}
+
+export default ButtonGroup
