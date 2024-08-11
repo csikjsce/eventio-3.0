8c0bd064f4ddd9d765aa6c640e688821bdc023a9
Unchanted <omkkdwivedi@gmail.com>
2024-08-11 17:17:58 +0530

    admin page
    sidebar, routing, icons for all projects, react-router-dom installation

--- frontend/council/src/App.tsx
+++ frontend/council/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import Sidebar from './components/sidebar.tsx';
+import Dashboard from './routes/dashboard.tsx';
+import Events from './routes/events.tsx';
+import Calendar from './routes/calendar.tsx';
+import Statistics from './routes/statistics.tsx';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+export default function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/stats" element={<Statistics />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
---
+++ frontend/council/src/components/sidebar.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Grid3, Gift, Calendar, Chart, Logout } from "iconsax-react";
+import Eventio from "../assets/Eventio.svg";
+
+const MenuItem = ({ Icon, href, children, isSelected }) => (
+  <Link to={href} className="hover:cursor-pointer hover:drop-shadow-xl">
+    <div
+      className={`font-poppins flex items-center gap-4 whitespace-nowrap ${
+        isSelected ? "text-[#b61f2d]" : "text-gray-600"
+      }`}
+    >
+      <Icon
+        size="26"
+        variant={isSelected ? "Bold" : "Linear"}
+        color={isSelected ? "#b61f2d" : "currentColor"}
+      />
+      <div className="flex-auto">{children}</div>
+    </div>
+  </Link>
+);
+
+export default function Sidebar() {
+  const location = useLocation();
+  const menuItems = [
+    {
+      icon: Grid3,
+      text: "Dashboard",
+      href: "/",
+    },
+    {
+      icon: Gift,
+      text: "Events",
+      href: "/events",
+    },
+    {
+      icon: Calendar,
+      text: "Calendar",
+      href: "/calendar",
+    },
+    {
+      icon: Chart,
+      text: "Statistics",
+      href: "/stats",
+    },
+  ];
+
+  return (
+    <div className="fixed top-0 left-0 h-full w-[250px] bg-[#f3f3f3] rounded-3xl px-[35px] py-[55px]">
+      <nav className="flex h-full flex-col justify-between">
+        <div>
+          <header className="flex justify-between gap-3.5 whitespace-nowrap">
+            <img
+              loading="lazy"
+              src={Eventio}
+              alt="Eventio logo"
+              className="my-auto aspect-[0.93] w-[39px]"
+            />
+            <div className="grow justify-center">
+              <h1 className="text-red font-marcellus text-center text-3xl">
+                Eventio
+              </h1>
+              <h2 className="text-md font-fira italic">By CSI-KJSCE</h2>
+            </div>
+          </header>
+          <div className="my-10 flex flex-col gap-10">
+            {menuItems.map((item, index) => (
+              <MenuItem
+                key={index}
+                Icon={item.icon}
+                href={item.href}
+                isSelected={location.pathname === item.href}
+              >
+                {item.text}
+              </MenuItem>
+            ))}
+          </div>
+        </div>
+        <footer className="flex items-center gap-4 text-gray-600 pb-[45px]">
+          <Logout size="26" variant="Bold" />
+          <div className="font-poppins">Logout</div>
+        </footer>
+      </nav>
+    </div>
+  );
+}
