0c844847aad35b2150fe67094b58afa5a277b14a
Om Lanke <oplanke@gmail.com>
2024-08-12 00:56:45 +0530

    prettier and eslint

---
+++ frontend/council/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "prettier/prettier": ["error", {
+      "semi": true,
+      "singleQuote": true
+    }]
+  }
+};
