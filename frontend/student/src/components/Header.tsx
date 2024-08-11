5d9d55b27ceb4b1ced41775086c3f4a3ce33bdc8
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-08-11 20:51:18 +0530

    Header

--- frontend/student/src/components/Header.tsx
+++ frontend/student/src/components/Header.tsx
@@ -1,27 +1,50 @@
-import React from "react";
-import { Notification } from "iconsax-react";
-import { Setting2 } from "iconsax-react";
-import abhi from "../assets/abhi.png";
-export const Header = (): JSX.Element => {
-  return (
-    <div className="flex w-[380px] items-center justify-between relative">
-      <div className="items-center gap-3 inline-flex relative flex-[0_0_auto]">
-        <img className="relative w-[48.5px] h-[50.5px]" alt="Image" src={abhi} />
-        <div className="flex-col h-[55px] items-start justify-center gap-1.5 inline-flex relative flex-[0_0_auto]">
-          <div className="relative w-fit mt-[-1.50px] font-primary font- text-constants-constants-gray text-[length:var(--primary-font-size)] text-center tracking-[var(--primary-letter-spacing)] leading-[var(--primary-line-height)] [font-style:var(--primary-font-style)]">
-            Namaste
-          </div>
-          <div className="relative w-fit font-primary font-[number:var(--primary-font-weight)] text-constants-foregroundcolor text-[length:var(--primary-font-size)] text-center tracking-[var(--primary-letter-spacing)] leading-[var(--primary-line-height)] [font-style:var(--primary-font-style)]">
-            Kunal Chaturvedi
-          </div>
+import React, {useState} from 'react';
+import { colors } from '../colors';
+import { Notification, Setting } from 'iconsax-react';
+
+const Header = (props: any) => {
+    
+    return (
+        <div className="w-full h-12 flex flex-row justify-between items-center">
+            <div className="flex flex-row items-center w-56 h-11 gap-3">
+                <div className="w-12 h-12" style={{borderRadius: 100}}>
+                    {props.profileImg && (
+                        <img
+                            className="w-full h-full"
+                            src={undefined}
+                            style={{borderRadius: 100, width: 48, height: 48}}
+                        />
+                    )}
+                </div>
+                <div className="flex justify-between">
+                    <div>
+                        <p
+                            className="text-gray text-xl font"
+                            style={{fontFamily: 'Marcellus-Regular'}}>
+                            Namaste
+                        </p>
+                    </div>
+                    <div>
+                        <p
+                            className="text-xl text-black dark:text-white capitalize"
+                            style={{fontFamily: 'Marcellus-Regular'}}>
+                            {props.name}
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex flex-row gap-6">
+                <button onClick={undefined}>
+                    <Notification color={colors.gray[1]} size={24} />
+                </button>
+
+                <button onClick={undefined}>
+                    <Setting color={colors.gray[1]} size={24} />
+                </button>
+            </div>
         </div>
-      </div>
-      <div className="items-center gap-5 inline-flex relative flex-[0_0_auto]">
-        <Notification className="!relative !w-6 !h-6" />
-        <Setting2 className="!relative !w-6 !h-6" />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Header;
