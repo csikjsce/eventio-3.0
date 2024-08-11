f345298910e740eb9862d3743fe5a459255ef2a9
aryan <aaryan.p114@gmail.com>
2024-08-11 15:03:55 +0530

    chore: fetching url from env

--- backend/main.js
+++ backend/main.js
@@ -20,7 +20,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:8000/api/v1/auth/google/callback",
+            callbackURL: process.env.SERVER_URL+process.env.GOOGLE_CALLBACK_URL,
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
