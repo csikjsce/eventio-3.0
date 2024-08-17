73e1f520b329300811e67adcf5a41f30f6f877a7
Suja16 <sujal16@somaiya.edu>
2024-08-17 17:15:39 +0530

    Login page

---
+++ frontend/student/src/login/page.tsx
@@ -0,0 +1,39 @@
+import { Button } from '@material-tailwind/react';
+import { useEffect } from 'react';
+
+export default function Login() {
+  // Function to initiate the Google login process
+  const login = () => {
+    window.open('http://localhost:8000/api/v1/auth/google', '_self');
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const accessTokenParam = urlParams.get('accessToken');
+    const refreshTokenParam = urlParams.get('refreshToken');
+
+    if (accessTokenParam && refreshTokenParam) {
+      // Store tokens in localStorage
+      localStorage.setItem('accessToken', accessTokenParam);
+      localStorage.setItem('refreshToken', refreshTokenParam);
+
+      // Clear the tokens from the URL
+      window.history.replaceState(null, '', window.location.pathname);
+    }
+  }, []);
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button
+        fullWidth
+        onClick={login}
+        className="max-w-xs bg-blue-500 text-white"
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+      >
+        Login with Google
+      </Button>
+    </div>
+  );
+}
