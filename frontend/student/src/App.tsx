99d0f772826e2ad238e42495f39278e5f9d74578
aryan <aaryan.p114@gmail.com>
2024-08-17 13:49:30 +0530

    feat: initialize user context

--- frontend/student/src/App.tsx
+++ frontend/student/src/App.tsx
@@ -1,27 +1,22 @@
+import { useContext, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import indexRoutes from './routes/routes';
-import ConditionalRoute from './routes/conditionalRoute';
+import { UserDataContext } from './contexts/userContext';
 
 function App() {
-  const isLoggedIn = true; // subject to change after building context
-
+  const [user, setUser] = useState<User | null>(null);
+  const userContext = useContext(UserDataContext)
+  userContext.userData = user
+  userContext.setUserData = setUser
   return (
-    <Routes>
-      {indexRoutes.map((prop, key) => {
-        return (
-          <Route
-            path={prop.path}
-            key={key}
-            element={
-              <ConditionalRoute condition={isLoggedIn} redirectTo="/">
-                {prop.component}
-              </ConditionalRoute>
-            }
-          ></Route>
-        );
-      })}
-      {/* <Route path="/404" element={<TechnicalError  />} /> */}
-    </Routes>
+      <Routes>
+        {indexRoutes.map((prop, key) => {
+          return (
+            <Route path={prop.path} key={key} element={prop.component}></Route>
+          );
+        })}
+        {/* <Route path="/404" element={<TechnicalError  />} /> */}
+      </Routes>
   );
 }
 
