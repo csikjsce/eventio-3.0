e4bca872de8778af5f2000b92af522f3a8e0d313
aryan <aaryan.p114@gmail.com>
2024-08-20 05:36:51 +0530

    fix: myevent page removed

--- frontend/student/src/screens/events/MyEvents.tsx
+++ frontend/student/src/screens/events/MyEvents.tsx
@@ -1,36 +1,36 @@
-import Header from '../../components/Header';
-import EventCard from '../../components/EventCard';
-import { ArrowLeft } from 'iconsax-react';
-import FooterNav from '../../components/FooterNav';
-import abhi from '../../assets/abhi.jpeg';
-import man1 from '../../assets/man1.jpeg';
-const event: {
-  name: string;
-  council: string;
-  image: string;
-  councilImage: string;
-  date: Date;
-  location: string;
-  tags: string[];
-  shortDesc: string;
-  status: string;
-} = {
-  name: 'Road To Programming',
-  council: 'CSI KJSCE',
-  image: abhi,
-  councilImage: man1,
-  date: new Date(1724758200000),
-  location: 'KJSCE Auditorium',
-  tags: ['Tech', 'Registered'],
-  shortDesc: 'Short Description',
-  status: 'Live',
-};
+// import Header from '../../components/Header';
+// import EventCard from '../../components/EventCard';
+// import { ArrowLeft } from 'iconsax-react';
+// import FooterNav from '../../components/FooterNav';
+// import abhi from '../../assets/abhi.jpeg';
+// import man1 from '../../assets/man1.jpeg';
+// const event: {
+//   name: string;
+//   council: string;
+//   image: string;
+//   councilImage: string;
+//   date: Date;
+//   location: string;
+//   tags: string[];
+//   shortDesc: string;
+//   status: string;
+// } = {
+//   name: 'Road To Programming',
+//   council: 'CSI KJSCE',
+//   image: abhi,
+//   councilImage: man1,
+//   date: new Date(1724758200000),
+//   location: 'KJSCE Auditorium',
+//   tags: ['Tech', 'Registered'],
+//   shortDesc: 'Short Description',
+//   status: 'Live',
+// };
 
 export default function MyEvents() {
   return (
     <>
       <div className="flex flex-col p-4">
-        <div className="flex flex-col gap-8 ">
+        {/* <div className="flex flex-col gap-8 ">
           <Header />
           <div className="flex flex-col gap-4 z-10">
             <p className="text-lg py-7 font-small font-fira text-left text-foreground-light dark:text-foreground-dark">
@@ -54,7 +54,7 @@ export default function MyEvents() {
             </div>
           </div>
         </div>
-        <FooterNav />
+        <FooterNav /> */}
       </div>
     </>
   );
