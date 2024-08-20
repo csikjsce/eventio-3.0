99b563dc0e60a81183348a4050ba223c1fb57692
Om Lanke <oplanke@gmail.com>
2024-08-20 20:11:12 +0530

    "mobile only" when on laptop

--- frontend/student/src/App.tsx
+++ frontend/student/src/App.tsx
@@ -1,14 +1,41 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import indexRoutes from './routes/routes';
 import { UserDataContext } from './contexts/userContext';
 import ConditionalRoute from './routes/conditionalRoute';
 
+import Loader from './components/Loader';
+
 function App() {
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
   const [user, setUser] = useState<User | null>(null);
   const userContext = useContext(UserDataContext);
   userContext.userData = user;
   userContext.setUserData = setUser;
+
+  useEffect(() => {
+    const handleResize = () => {
+        if (window.innerWidth > 720) {
+            setIsMobile(true);
+        } else {
+            setIsMobile(false);
+        }
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+}, []);
+
+  if (isMobile === null) {
+    return <Loader />;
+  } else if (isMobile) {
+    return (
+      <div className='flex flex-col h-screen justify-center items-center gap-8'>
+      <p className='font-poppins text-3xl text-primary '>Mobile Required</p>
+      <p className='font-poppins text-xl text-foreground-light'>Please use a mobile device and install the app to continue.</p>
+      </div>
+    )
+  }
 
   return (
     <Routes>
