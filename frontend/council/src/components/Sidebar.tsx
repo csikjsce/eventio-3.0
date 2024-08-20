6038b69ccfd460059c0ec1c4491bdf4561b5e9b6
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2024-08-20 14:05:35 +0530

    ran format and lint error

--- frontend/council/src/components/Sidebar.tsx
+++ frontend/council/src/components/Sidebar.tsx
@@ -1,86 +1,94 @@
 import { NavLink } from "react-router-dom";
 import { Grid3, Gift, Calendar, Logout, Icon as IconType } from "iconsax-react";
 import Eventio from "../assets/Eventio.svg";
-import PopoverCustomAnimation from '../components/PopoverButton.tsx';
+import PopoverCustomAnimation from "../components/PopoverButton.tsx";
 
-const MenuItem = ({ Icon, href, children }:
-  { Icon: IconType, href: string, children: string }
-) => (
-  <NavLink to={href} className="hover:cursor-pointer hover:drop-shadow-xl">
-    {({ isActive }) => (
-      <div
-        className={`font-poppins flex items-center gap-4 whitespace-nowrap ${
-          isActive ? "text-[#b61f2d]" : "text-gray-600"
-        }`}
-      >
-        <Icon
-          size="26"
-          variant={isActive ? "Bold" : "Linear"}
-          color={isActive ? "#b61f2d" : "currentColor"}
-        />
-        <div className="flex-auto">{children}</div>
-      </div>
-    )}
-  </NavLink>
+const MenuItem = ({
+    Icon,
+    href,
+    children,
+}: {
+    Icon: IconType;
+    href: string;
+    children: string;
+}) => (
+    <NavLink to={href} className="hover:cursor-pointer hover:drop-shadow-xl">
+        {({ isActive }) => (
+            <div
+                className={`font-poppins flex items-center gap-4 whitespace-nowrap ${
+                    isActive ? "text-[#b61f2d]" : "text-gray-600"
+                }`}
+            >
+                <Icon
+                    size="26"
+                    variant={isActive ? "Bold" : "Linear"}
+                    color={isActive ? "#b61f2d" : "currentColor"}
+                />
+                <div className="flex-auto">{children}</div>
+            </div>
+        )}
+    </NavLink>
 );
 
 export default function Sidebar() {
-  const menuItems = [
-    {
-      icon: Grid3,
-      text: "Dashboard",
-      href: "/",
-    },
-    {
-      icon: Gift,
-      text: "Events",
-      href: "/events",
-    },
-    {
-      icon: Calendar,
-      text: "Calendar",
-      href: "/calendar",
-    },
-  ];
+    const menuItems = [
+        {
+            icon: Grid3,
+            text: "Dashboard",
+            href: "/",
+        },
+        {
+            icon: Gift,
+            text: "Events",
+            href: "/events",
+        },
+        {
+            icon: Calendar,
+            text: "Calendar",
+            href: "/calendar",
+        },
+    ];
 
-  return (
-    <div className="h-[96vh] w-64 bg-[#f3f3f3] rounded-2xl px-9 py-14 my-auto">
-      <nav className="flex h-full flex-col justify-between">
-        <div>
-          <header className="flex justify-between gap-3.5 whitespace-nowrap">
-            <img
-              loading="lazy"
-              src={Eventio}
-              alt="Eventio logo"
-              className="my-auto aspect-[0.93] w-[39px]"
-            />
-            <div className="grow justify-center">
-              <h1 className="text-red font-marcellus text-center text-3xl">
-                Eventio
-              </h1>
-              <h2 className="text-md text-center font-fira italic">By CSI-KJSCE</h2>
-            </div>
-          </header>
-          <div className="my-10 flex flex-col gap-10">
-            {menuItems.map((item, index) => (
-              <MenuItem
-                key={index}
-                Icon={item.icon}
-                href={item.href}
-              >
-                {item.text}
-              </MenuItem>
-            ))}
-            <div className="font-poppins flex items-center gap-4 whitespace-nowrap text-gray-600">
-              <PopoverCustomAnimation />
-            </div>
-          </div>
+    return (
+        <div className="h-[96vh] w-64 bg-[#f3f3f3] rounded-2xl px-9 py-14 my-auto">
+            <nav className="flex h-full flex-col justify-between">
+                <div>
+                    <header className="flex justify-between gap-3.5 whitespace-nowrap">
+                        <img
+                            loading="lazy"
+                            src={Eventio}
+                            alt="Eventio logo"
+                            className="my-auto aspect-[0.93] w-[39px]"
+                        />
+                        <div className="grow justify-center">
+                            <h1 className="text-red font-marcellus text-center text-3xl">
+                                Eventio
+                            </h1>
+                            <h2 className="text-md text-center font-fira italic">
+                                By CSI-KJSCE
+                            </h2>
+                        </div>
+                    </header>
+                    <div className="my-10 flex flex-col gap-10">
+                        {menuItems.map((item, index) => (
+                            <MenuItem
+                                key={index}
+                                Icon={item.icon}
+                                href={item.href}
+                            >
+                                {item.text}
+                            </MenuItem>
+                        ))}
+                        <div className="font-poppins flex items-center gap-4 whitespace-nowrap text-gray-600">
+                            <PopoverCustomAnimation />
+                        </div>
+                    </div>
+                </div>
+                <footer className="flex items-center gap-4 text-gray-600 pb-12">
+                    <Logout size="26" variant="Bold" />
+                    <div className="font-poppins">Logout</div>
+                </footer>
+            </nav>
         </div>
-        <footer className="flex items-center gap-4 text-gray-600 pb-12">
-          <Logout size="26" variant="Bold" />
-          <div className="font-poppins">Logout</div>
-        </footer>
-      </nav>
-    </div>
-  );
+    );
 }
--- frontend/council/src/routes/events.tsx
+++ frontend/council/src/routes/events.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export default function Events() {
-  return(
-    <p>events</p>
-  )
+    return <p>events</p>;
 }
