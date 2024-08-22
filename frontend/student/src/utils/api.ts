26978d99936a79e71cc13889a59b79a2053c6ead
Om Lanke <oplanke@gmail.com>
2024-08-22 21:00:37 +0530

    add refreshtoken function

--- frontend/student/src/utils/api.ts
+++ frontend/student/src/utils/api.ts
@@ -1,9 +1,25 @@
 import axios from 'axios';
 
+
+
+async function refreshToken() {
+  try {
+    const response = await axios.request({
+      baseURL: import.meta.env.VITE_APP_SERVER_ADDRESS,
+      url: '/api/v1/auth/refresh-token',
+      method: 'POST',
+      data: {refreshToken: localStorage.getItem('refreshToken')},
+    });
+    localStorage.setItem('accessToken', response.data.accessToken);
+  } catch (err) {
+    throw err;
+  }
+}
+
 export const axiosCall = (
   method: 'GET' | 'POST',
   path: string,
-  sendToken: true,
+  sendToken: boolean,
   data?: object | null,
 ): Promise<UserResponse> => {
   return new Promise<UserResponse>((resolve, reject) => {
@@ -23,6 +39,24 @@ export const axiosCall = (
         resolve(response.data);
       })
       .catch((err) => {
+        console.debug(err);
+        if (
+          err.response &&
+          err.response.status === 401
+        ) {
+          console.log('Access token expired');
+          refreshToken().then(() => {
+            axiosCall(method, path, sendToken, data)
+              .then((response) => {
+                resolve(response);
+              })
+              .catch((err) => {
+                reject(err);
+              });
+          }).catch((err) => {
+            reject(err);
+          });
+        }
         if (err.response && err.response.data) {
           reject(err.response.data as UserResponse);
         }
