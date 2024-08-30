7aeca0feb9e5d8544d6065f2e9311a1fc94959ae
Om Lanke <oplanke@gmail.com>
2024-08-30 18:59:37 +0530

    nuke council portal

--- frontend/council/src/App.tsx
+++ frontend/council/src/App.tsx
@@ -1,27 +1,35 @@
-import React from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import Sidebar from "./components/Sidebar.tsx";
-import Dashboard from "./routes/dashboard.tsx";
-import Events from "./routes/events.tsx";
-import Calendar from "./routes/calendar.tsx";
-import Statistics from "./routes/statistics.tsx";
+function App() {
+  const [count, setCount] = useState(0)
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-export default function App() {
-    return (
-        <Router>
-            <div className="w-screen p-4 flex flex-row gap-4 border-4 border-red-500">
-                <Sidebar />
-                <div>
-                    <Routes>
-                        <Route path="/" element={<Dashboard />} />
-                        <Route path="/events" element={<Events />} />
-                        <Route path="/calendar" element={<Calendar />} />
-                        <Route path="/stats" element={<Statistics />} />
-                    </Routes>
-                </div>
-            </div>
-        </Router>
-    );
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
--- frontend/council/src/routes/statistics.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Statistics() {
-    return <p>Statistics</p>;
-}
