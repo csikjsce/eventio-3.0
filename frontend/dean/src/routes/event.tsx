13279a35a00bebc9811d165a957da11edcec9e8e
Anushlinux <anushrutpandit@gmail.com>
2024-09-02 15:37:33 +0530

    basic-shit-ui

---
+++ frontend/dean/src/routes/event.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EventHead from '../components/EventHeader'
+import Cards from '../components/Cards'
+
+const event = () => {
+    return (
+        <div>
+            <EventHead />
+            <Cards />
+        </div>
+    );
+};
+
+export default event;
