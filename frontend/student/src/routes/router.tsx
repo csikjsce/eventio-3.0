4635052948bb46683e220f5b4652ff66976f1e68
Om Lanke <oplanke@gmail.com>
2024-09-07 22:00:35 +0530

    lazy loading pages

--- frontend/student/src/routes/router.tsx
+++ frontend/student/src/routes/router.tsx
@@ -8,37 +8,78 @@ import HeaderLayout from '../layouts/HeaderLayout';
 import FooterLayout from '../layouts/FooterLayout';
 import Protected from '../layouts/Protected';
 
-import Login from '../login/page';
-import Logout from '../screens/logout/page';
-
-import GetStarted from '../screens/getStarted/page';
-
-import Home from '../screens/home/home';
-import Calendar from '../screens/calendar/Calendar';
-import Councils from '../screens/councils/Councils';
-import Profile from '../screens/profile/Profile';
-
-import EventDetails from '../screens/events/EventDetails';
-import Ticket from '../screens/ticket/tickets';
-
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route Component={Protected}>
         <Route Component={FooterLayout}>
           <Route Component={HeaderLayout}>
-            <Route index Component={Home} />
-            <Route path="calendar" Component={Calendar} />
-            <Route path="councils" Component={Councils} />
+            <Route
+              index
+              lazy={async () => {
+                const Home = await import('../screens/home/home');
+                return { Component: Home.default };
+              }}
+            />
+            <Route
+              path="calendar"
+              lazy={async () => {
+                const Calendar = await import('../screens/calendar/Calendar');
+                return { Component: Calendar.default };
+              }}
+            />
+            <Route
+              path="councils"
+              lazy={async () => {
+                const Councils = await import('../screens/councils/Councils');
+                return { Component: Councils.default };
+              }}
+            />
           </Route>
-          <Route path="profile" Component={Profile} />
+          <Route
+            path="profile"
+            lazy={async () => {
+              const Profile = await import('../screens/profile/Profile');
+              return { Component: Profile.default };
+            }}
+          />
         </Route>
-        <Route path="event-details/:id" Component={EventDetails} />
-        <Route path="ticket/:id" Component={Ticket} />
-        <Route path="getting-started" Component={GetStarted} />
+        <Route
+          path="event-details/:id"
+          lazy={async () => {
+            const EventDetails = await import('../screens/events/EventDetails');
+            return { Component: EventDetails.default };
+          }}
+        />
+        <Route
+          path="ticket/:id"
+          lazy={async () => {
+            const Tickets = await import('../screens/ticket/tickets');
+            return { Component: Tickets.default };
+          }}
+        />
+        <Route
+          path="getting-started"
+          lazy={async () => {
+            const GetStarted = await import('../screens/getStarted/page');
+            return { Component: GetStarted.default };
+          }}
+        />
       </Route>
-      <Route path="login" Component={Login} />
-      <Route path="logout" Component={Logout} />
+      <Route
+        path="login"
+        lazy={async () => {
+          const Login = await import('../login/page');
+          return { Component: Login.default };
+        }}
+      />
+      <Route
+        path="logout"
+        lazy={async () => {
+          const Logout = await import('../screens/logout/page');
+          return { Component: Logout.default };
+        }}
+      />
     </Route>,
   ),
 );
