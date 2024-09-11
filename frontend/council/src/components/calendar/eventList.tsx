fea1440b88f4e5a3136db7a1cfba79183ec04ebf
Unchanted <omkkdwivedi@gmail.com>
2024-09-11 23:51:50 +0530

    feat(council): council dashboard, calendar added

---
+++ frontend/council/src/components/calendar/eventList.tsx
@@ -0,0 +1,29 @@
+import dayjs, { Dayjs } from 'dayjs';
+import Event from './event';
+
+type Props = {
+  events: EventData[];
+  date: Dayjs;
+};
+
+const EventList = ({ events, date }: Props) => {
+  return (
+    <div className="pb-20 ">
+      {events.map((event, index) => (
+        <Event
+          key={index}
+          council={event.organizer.name}
+          date={date}
+          title={event.name}
+          image={event.banner_url}
+          id={event.id}
+          startTime={dayjs(
+            event.dates.find((d) => date.isSame(d, 'day')),
+          ).format('hh:mm A')}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default EventList;
