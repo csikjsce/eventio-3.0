192147da2d8a5814ccce30e7e3b18c1c33949d08
Anushlinux <anushrutpandit@gmail.com>
2024-09-12 00:03:09 +0530

    Events page done

--- frontend/dean/src/App.tsx
+++ frontend/dean/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
       <Router>
-          <div className=" p-4 flex flex-row gap-4  ">
+          <div className="p-4 flex flex-row gap-4">
               <div>
                   <Sidebar />
                   <Routes>
--- frontend/dean/src/components/sidebar.tsx
+++ frontend/dean/src/components/sidebar.tsx
@@ -88,8 +88,6 @@ export default function Sidebar() {
                     </footer>
                 </nav>
             </div>
-
-            
         </>
     );
 }
